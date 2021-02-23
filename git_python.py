5e6899d9f300dd018e74cd0461d879ce6944f9b7
99003767 <darpan.patil@ltts.com>
2021-02-23 12:45:45 +0530

    updated

--- git_python.py
+++ git_python.py
@@ -1,5 +1,17 @@
-print("git_python is working")
-print("YO")
-print("Cloned in Pycharm")
-print("Opened vs code in linux")
-print("opened in pycharm in linux")
+import re
+count = 0
+software = []
+lines = 0
+
+pattern = re.compile("Software",re.I)
+
+with open ('input.txt','rt') as file_info:
+    for file_line in file_info:
+        lines +=1
+        if pattern.search(file_line) != None:
+            software.append((lines,file_line.rstrip('\n')))
+    for answer in software:
+        count =+1
+        with open("software.txt",'a') as file_answer:
+            file_answer.writelines(str(count)+' :')
+            file_answer.writelines(answer[1]+'\n')      
