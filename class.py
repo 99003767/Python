eff91ee9ac10177268348e5d0faf3c9683a64d28
99003767 <darpan.patil@ltts.com>
2021-02-24 15:15:22 +0530

    update

--- class.py
+++ class.py
@@ -1,16 +1,36 @@
 import re
 
+count=1
+
+
 input_file=open("input.txt", "r")
+
 read_file =input_file.read()
+
 find_word=input("ENTER THE WORD YOU WANT TO SEARCH: ")
+
 x=re.findall(find_word,  read_file, re.M|re.I)
+input_file1=read_file.split()
 
 file_save= find_word+'.txt'
 write_file=open(file_save,'w+')
-#write_file.write(str(x)+'\n')
+
+for i in range(len(input_file1)):
+    word = re.match(find_word, input_file1[i], re.M | re.I)
+    if word:
+        count+=1
+       
+        str1=(input_file1[i-1]+' '+ input_file1[i]+ ' '+input_file1[i+1])
+        write_file.write(str(str1) + '\n')
+        write_file.write(str(count) + ' :')
+
+
 write_file.write('No. of time word repeated:'+ str(len(x))+'\n')
-with open("input.txt") as openfile:
-    for line in openfile:
-        for part in line.split():
-            if find_word in part:
-                write_file.write(str(part)+'\n')
+#with open("input.txt") as openfile:
+    #for line in openfile:
+        #for part in line.split():
+
+            #if find_word in part:
+                #count += 1
+                #
+                #write_file.write(str(part)+'\n')
