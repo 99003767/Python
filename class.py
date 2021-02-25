a9230aac5e0b0e852688fe6cd364da8b7c90e00e
99003767 <darpan.patil@ltts.com>
2021-02-25 12:36:22 +0530

    update

--- class.py
+++ class.py
@@ -1,18 +1,37 @@
 import re
-count = 0
-input_file = open("input.txt", 'r')
-read_file = input_file.read()
-main_file = read_file.split()
-find_word = input("Enter the word to be search:")
-word = re.findall(find_word, read_file, re.M | re.I)
-print(len(word))
-file_save = find_word+'.txt'
 
-write_file = open(file_save, 'w+')
-for x in range(len(main_file)):
-    matching_done = re.match(find_word,  main_file[x], re.M | re.I)
-    if matching_done:
-        count += 1
-        y = (main_file[x-1] + ' ' + main_file[x]+' '+main_file[x+1] + '\n')
-        write_file.writelines(str(y))
-write_file.writelines("the Total count of words :"+str(count))
+
+class SearchWord:
+
+    def __init__(self, keyword_given, file_text):
+        self.keyword_given = keyword_given
+        self.file_text = file_text
+
+    def keyword_search(self,):
+
+        counting = 0
+        match = re.findall(self.keyword_given, self.file_text, re.M | re.I)
+        extension = ".txt"
+        file_name = str(self.keyword_given)+extension
+        file_creation = open(file_name, "w+")
+        file_creation.write("The number of occurrences of keyword is:" + str(len(match)))
+        file_creation.write(str("\n\n"))
+        split_file_text = self.file_text.split()
+        f = open(file_name, "a+")
+        for iteration in range(len(split_file_text)):
+            matching = re.match(self.keyword_given, split_file_text[iteration], re.M | re.I)
+            if matching:
+                counting += 1
+                y = (split_file_text[iteration-1]+" "+split_file_text[iteration]+" "+split_file_text[iteration+1]+"\n")
+                f.writelines(str(y))
+
+
+if __name__ == '__main__':
+
+    with open('input.txt') as input_file:
+        read_file = input_file.read()
+        print(read_file)
+    for x in range(5):
+        keyword = input("Enter the keyword you wanted to search in file:")
+        d = SearchWord(keyword, read_file)
+        d.keyword_search()
