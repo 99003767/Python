696633f3b54c4077ecfeb844f7d2f197a6e0e3d2
99003767 <darpan.patil@ltts.com>
2021-02-25 17:00:12 +0530

    update

--- class.py
+++ class.py
@@ -1,37 +1,44 @@
-import re
+'''
+Miniproject for python
+Author:Darpan Patil
+PS no: 99003767
+email: darpan.patil@ltts.com
+date:25-02-2020
+'''
+import re                                                #importing regular expressions package
 
 
-class Mini_project:
+class Mini_project:                                      #declaring class
 
-    def __init__(self, keyword, file):
+    def __init__(self, keyword, file):                    #initializing variables
         self.keyword = keyword
         self.file = file
 
-    def keyword_find(self,):
+    def keyword_find(self,):                             #Function to search 
 
-        count = 0
-        match = re.findall(self.keyword, self.file, re.M | re.I)
-        extension = ".txt"
-        file_name = str(self.keyword)+extension
-        file_creation = open(file_name, "w+")
-        file_creation.write("The number of occurrences of keyword is:" + str(len(match)))
-        file_creation.write(str("\n\n"))
-        split_file_text = self.file.split()
-        f = open(file_name, "a+")
-        for iteration in range(len(split_file_text)):
+        count = 0                                          #variable to keep the count
+        match = re.findall(self.keyword, self.file, re.M | re.I)        #line used to find word
+        extension = ".txt"                                              
+        file_name = str(self.keyword)+extension                      #line to save the file
+        file_creation = open(file_name, "w+")                          # open the file in write mode
+        file_creation.write("The number of occurrences of keyword is:" + str(len(match)))       #line which prints the count
+        file_creation.write(str("\n\n"))                                  #for next line
+        split_file_text = self.file.split()                               #for splitting the lines
+        f = open(file_name, "a+")                                         #opening file in append mode
+        for iteration in range(len(split_file_text)):                     #for loop for appending       
             matching = re.match(self.keyword, split_file_text[iteration], re.M | re.I)
             if matching:
                 count += 1
-                y = (split_file_text[iteration-1]+" "+split_file_text[iteration]+" "+split_file_text[iteration+1]+"\n")
-                f.writelines(str(y))
+                y = (split_file_text[iteration-1]+" "+split_file_text[iteration]+" "+split_file_text[iteration+1]+"\n")  #finding the previous and next word
+                f.writelines(str(y))                                       #writing those lines to file
 
 
 if __name__ == '__main__':
 
-    with open('input.txt') as input_file:
-        read_file = input_file.read()
+    with open('input.txt') as input_file:                               #opening the input file
+        read_file = input_file.read()                                   #reading the file   
         print(read_file)
-    for x in range(5):
-        keywordd = input("Enter the keyword you wanted to search in file:")
-        d = Mini_project(keywordd, read_file)
+    for x in range(5):                                                  #loop for searching the words
+        keywordd = input("Enter the keyword you wanted to search in file:")       #user giving the input for the word to search
+        d = Mini_project(keywordd, read_file)                                    #passing the parameters to function
         d.keyword_find()
