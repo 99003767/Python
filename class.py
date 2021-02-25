7e833cd7fe1666f0e14ca73782146c262001e706
99003767 <darpan.patil@ltts.com>
2021-02-25 16:36:35 +0530

    update

--- class.py
+++ class.py
@@ -1,27 +1,27 @@
 import re
 
 
-class SearchWord:
+class Mini_project:
 
-    def __init__(self, keyword_given, file_text):
-        self.keyword_given = keyword_given
-        self.file_text = file_text
+    def __init__(self, keyword, file):
+        self.keyword = keyword
+        self.file = file
 
-    def keyword_search(self,):
+    def keyword_find(self,):
 
-        counting = 0
-        match = re.findall(self.keyword_given, self.file_text, re.M | re.I)
+        count = 0
+        match = re.findall(self.keyword, self.file, re.M | re.I)
         extension = ".txt"
-        file_name = str(self.keyword_given)+extension
+        file_name = str(self.keyword)+extension
         file_creation = open(file_name, "w+")
         file_creation.write("The number of occurrences of keyword is:" + str(len(match)))
         file_creation.write(str("\n\n"))
-        split_file_text = self.file_text.split()
+        split_file_text = self.file.split()
         f = open(file_name, "a+")
         for iteration in range(len(split_file_text)):
-            matching = re.match(self.keyword_given, split_file_text[iteration], re.M | re.I)
+            matching = re.match(self.keyword, split_file_text[iteration], re.M | re.I)
             if matching:
-                counting += 1
+                count += 1
                 y = (split_file_text[iteration-1]+" "+split_file_text[iteration]+" "+split_file_text[iteration+1]+"\n")
                 f.writelines(str(y))
 
@@ -32,6 +32,6 @@ if __name__ == '__main__':
         read_file = input_file.read()
         print(read_file)
     for x in range(5):
-        keyword = input("Enter the keyword you wanted to search in file:")
-        d = SearchWord(keyword, read_file)
-        d.keyword_search()
+        keywordd = input("Enter the keyword you wanted to search in file:")
+        d = Mini_project(keywordd, read_file)
+        d.keyword_find()
